fc1806d031cff9c70de70ee663d00e32551e5a47
ritik-agrawal <ritikagrawal1697@gmail.com>
2024-07-13 19:14:59 +0530

    error

--- product-service/src/main/resources/db/changelog/changes/11_positive_pincode.sql
+++ product-service/src/main/resources/db/changelog/changes/11_positive_pincode.sql
@@ -6,4 +6,4 @@ INSERT INTO `system_config`
 (`id`, `created_at`, `created_by`, `deleted`, `modified_at`, `modified_by`, `config_key`, `config_value`)
 VALUES
 (8, '2024-05-07 05:29:17', 'ritik', 0, '2024-05-07 05:29:17', 'ritik', 'positive.pincode.available', 'false'),
-(9, '2024-05-07 05:29:17', 'ritik', 0, '2024-05-07 05:29:17', 'ritik', 'positive.pincode.list', '766001, 400021');0
+(9, '2024-05-07 05:29:17', 'ritik', 0, '2024-05-07 05:29:17', 'ritik', 'positive.pincode.list', '766001, 400021');
